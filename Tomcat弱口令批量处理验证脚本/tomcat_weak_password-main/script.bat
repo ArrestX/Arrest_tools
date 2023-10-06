f4a772ebe6280b01846fb35fcd40cdc7d062477f
arrest <hansl666@126.com>
2023-10-06 17:44:13 +0800

    对py脚本和java脚本进行分类

--- Tomcat弱口令批量处理验证脚本/tomcat_weak_password-main/script.bat
+++
@@ -1,6 +0,0 @@
-setlocal enabledelayedexpansion
-for /f "tokens=1-3 delims=," %%i in (ip.txt) do (
-    set name=%%i
-    start cmd /c  python tomcat_weak_password.py -u !name!
-
-)
--- Tomcat弱口令批量处理验证脚本/tomcat_weak_password-main/测试脚本/test.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-for /f "tokens=1-3 delims=," %%i in (tmp2.txt) do (
-    set name=%%i
-    set age=%%j
-    set address=%%k
-    echo name=!name!   age=!age!   address=!address!
-)
-pause
